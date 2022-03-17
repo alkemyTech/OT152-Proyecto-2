8393ad06ca18aa373a77812efcf546ea730a6445
Tadeo Vardánega <tadeo.vardanega@gmail.com>
2022-03-17 20:02:50 -0300

    primer commit

---
+++ Grupo_datos_G/main.py
@@ -0,0 +1,10 @@
+import logging
+import logging.config
+from decouple import config as cfg
+
+# Load logging configurations
+logging.config.fileConfig(cfg('FILE_PATH'))
+# Instansiate logger class
+logger = logging.getLogger('Grupo_Datos_G')
+
+logger.info('test')
