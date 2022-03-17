e11e91eb828f7e954844da95260a8bc796a832cf
Joel Solaligue <solaligue.joel98@gmail.com>
2022-03-17 17:30:01 -0300

    correcting flek8 configs

--- Grupo_Datos_E/main.py
+++ Grupo_Datos_E/main.py
@@ -1,9 +1,8 @@
 from decouple import config
-import logging
-from logging.config import fileConfig
+import logging.config
 
 cfg_logging = config('CFG_PATH')
 
-fileConfig(cfg_logging)
+logging.config.fileConfig(cfg_logging)
 
 logger = logging.getLogger('log_datos_e')
