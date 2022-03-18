d8b2a09f0b6622155c524cdd0118e5eb7dcf3022
Gian Arias <ariasgian@gmail.com>
2022-03-17 21:25:58 -0300

    update

--- grupo_datos_B/main.py
+++ grupo_datos_B/main.py
@@ -1,6 +1,8 @@
 import logging
 import logging.config
+from decouple import config as cfg
 
-logging.config.fileConfig('logging.cfg')
+logging.config.fileConfig(cfg('FILE_PATH'))
+#logging.config.fileConfig('logging.cfg')
 logger = logging.getLogger('root')
 logger.info('inicio')
