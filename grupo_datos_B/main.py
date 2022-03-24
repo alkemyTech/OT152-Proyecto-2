1e12e263a87cd80b947fbcb6d451002c47852b6f
Gian Arias <ariasgian@gmail.com>
2022-03-23 22:42:03 -0300

    insert function necesary

--- grupo_datos_B/main.py
+++ grupo_datos_B/main.py
@@ -6,4 +6,28 @@ import os
 
 base = str(os.path.dirname(__file__)) + '/'
 print(base)
-ef = ET.parse(base + 'posts.xml')
+tree = ET.parse(base + 'posts.xml')
+
+root = tree.getroot()
+
+
+def respuestas_aceptadas(data):
+    try:
+        salida = data.attrib['AcceptedAnswerId']
+    except:
+        return
+    tag = data.attrib['Tags']
+    tag = re.findall('<(.+?)>', tag)
+    return tag, salida
+
+
+def chunkify(iterable, len_of_chunk):
+    for i in range(0, len(iterable), len_of_chunk):
+        yield iterable[i:i + len_of_chunk]
+
+
+datos_cortados = chunkify(root, 10)
+mapped = list(map(respuestas_aceptadas, root))
+print(len(mapped))
+mapped1 = list(filter(None, mapped))
+print(len(mapped1))
