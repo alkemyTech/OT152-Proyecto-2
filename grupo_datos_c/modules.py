b9522ff149e1f9ed7c4950c04bcd4c35913920ae
JoacoAnun <joaco.anun@gmail.com>
2022-03-25 17:39:44 -0300

    Added functions to process data and display words/awnsercount ratio of posts

--- grupo_datos_c/modules.py
+++ grupo_datos_c/modules.py
@@ -1,12 +1,14 @@
 from collections import Counter
+import re
 
 
+# Functions for top 10 tags ------------------------------------
 def get_tags_and_ansid(data_chunk):
     """
     Returns two lists with all tags and accepdet answer id.
 
         Parameters:
-                data_chunk (generator): Generator objetc with the data chunk to process
+                data_chunk (list): c with the data chunk to process
         Returns:
                 tags, acc_ans_id (lists): lists containing data requested
     """
@@ -24,7 +26,7 @@ def get_tags_with_acc_answ(data):
     Returns a list with tags that only have accepted answer id
 
         Parameter:
-                data (map object): Contains two lists with tags and accepted answer id
+                data (list): Contains two lists with tags and accepted answer id
         Returns:
                 tags_with_acc_ans (list): with only tags that have accepted answer id
     """
@@ -42,7 +44,7 @@ def split_and_count_tags(tags):
     """
     Returns a list with Counter object with dictionares counting all tags per chunk of data
 
-            Parameter tags (map object):
+            Parameter tags (list):
                     Contains tags with accepted answer id
             Returns:
                     counter (list): with all Counter objects
@@ -60,3 +62,84 @@ def split_and_count_tags(tags):
         counter.update(Counter(tag_string.split()))
 
     return counter
+
+
+# Functions for word/answercount ratio--------------------
+def get_bodies_and_answer_counts(data_chunk):
+    """
+    Returns three lists with id, bodie of post and awnser count
+
+            Parameters:
+                    data_chunk (list): Generator objetc with the data chunk to process
+            Returns:
+                    post_id, bodies, answer_counts(lists): lists containing data requested
+    """
+    post_id = []
+    bodies = []
+    answer_counts = []
+
+    for data in data_chunk:
+        post_id.append(data.attrib.get('Id'))
+        bodies.append(data.attrib.get('Body'))
+        answer_counts.append(data.attrib.get('AnswerCount'))
+
+    return post_id, bodies, answer_counts
+
+
+def bodies_with_answer_count(data):
+    """
+    Returns id, body and answer count, if answer count is not None or 0
+
+            Parameter:
+                    data (list): Contains id, body and answer count
+            Returns:
+                    bodies_with_answers (list): contains id, body and answer count
+    """
+    bodies_with_answers = []
+    data = zip(data[0], data[1], data[2])
+
+    for post_id, body, answer_count in data:
+        # Check if answer count exist or if it's not 0
+        if answer_count is not None:
+            if int(answer_count):
+                bodies_with_answers.append([int(post_id), body, int(answer_count)])
+
+    return bodies_with_answers
+
+
+def count_words_in_body(data):
+    """
+    Returns a list with id, word count and answer count
+
+            Parameter:
+                    data (list): Contains id, body and answer count
+            Returns:
+                    bodies_with_answers (list): contains id, word count and answer count
+    """
+    for i in range(len(data)):
+        body = data[i][1]
+        html_patterns = "<(?:\"[^\"]*\"['\"]*|'[^']*'['\"]*|[^'\">])+>"
+        # Subtract html patterns from the string
+        body = re.sub(html_patterns, '', body)
+        # Calculate the total words from each body and reasing the data
+        data[i][1] = len(re.findall(r'\d+\.\d+|\w+-?\w+|\w', body))
+
+    return data
+
+
+def generate_data_dict(data):
+    """
+    Returns a dictionary with post id as key,
+              and value is a dictionary with Word count, Answer count and Words per answer ratio
+
+            Parameter:
+                    data (list): Contains id, body and answer count
+            Returns:
+                    bodies_with_answers (list): contains id, word count and answer count
+    """
+    data_dict = {}
+    for post_id, word_count, answer_count in data:
+        data_dict.update({post_id: {'Word count': word_count,
+                                    'Answer count': answer_count,
+                                    'Words per answers ratio': word_count / answer_count}})
+    return data_dict
