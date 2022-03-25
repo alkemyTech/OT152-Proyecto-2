bd8afeb24e44ec41b733cef3d3cbc4ef0cf67d87
Gian Arias <ariasgian@gmail.com>
2022-03-25 12:56:38 -0300

    test separate tags

--- grupo_datos_B/main.py
+++ grupo_datos_B/main.py
@@ -11,14 +11,15 @@ tree = ET.parse(base + 'posts.xml')
 root = tree.getroot()
 
 
-def respuestas_aceptadas(data):
+def respuestas_sin_respuesta(data):
     try:
         salida = data.attrib['AcceptedAnswerId']
     except:
         return
     tag = data.attrib['Tags']
     tag = re.findall('<(.+?)>', tag)
-    return tag, salida
+    counter_tag = Counter(tag)
+    return counter_tag
 
 
 def chunkify(iterable, len_of_chunk):
@@ -26,8 +27,21 @@ def chunkify(iterable, len_of_chunk):
         yield iterable[i:i + len_of_chunk]
 
 
+def reducir_contadores(data1, data2):
+    for key, value in data2.items():
+        if key in data1.keys():
+            data1[key].update(data2[key])
+        else:
+            data1.update({key: value})
+    return data1
+
+def separar_tags(data):
+    for key in data.items():
+        return dict([(key[0], key[1])])
+    # return dict([(tag[0], tag[1]) for tag in data.items()])
+
 datos_cortados = chunkify(root, 10)
-mapped = list(map(respuestas_aceptadas, root))
-print(len(mapped))
+mapped = list(map(respuestas_sin_respuesta, root))
 mapped1 = list(filter(None, mapped))
-print(len(mapped1))
+tag_palabra= list(map(separar_tags, mapped1))
+reducido = reduce(reducir_contadores, tag_palabra)
