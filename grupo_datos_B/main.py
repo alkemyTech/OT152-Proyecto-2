5222716845851b05b92b6e5beaf4ffe566c10160
Gian Arias <ariasgian@gmail.com>
2022-03-26 11:11:49 -0300

    clean line

--- grupo_datos_B/main.py
+++ grupo_datos_B/main.py
@@ -4,17 +4,11 @@ import xml.etree.ElementTree as ET
 import re
 import os
 
-base = str(os.path.dirname(__file__)) + '/'
-print(base)
-tree = ET.parse(base + 'posts.xml')
 
-root = tree.getroot()
-
-
-def respuestas_sin_respuesta(data):
+def post_sin_respuesta(data):
     try:
-        salida = data.attrib['AcceptedAnswerId']
-    except:
+        data.attrib['AcceptedAnswerId']
+    except Exception:
         return
     tag = data.attrib['Tags']
     tag = re.findall('<(.+?)>', tag)
@@ -22,26 +16,30 @@ def respuestas_sin_respuesta(data):
     return counter_tag
 
 
-def chunkify(iterable, len_of_chunk):
-    for i in range(0, len(iterable), len_of_chunk):
-        yield iterable[i:i + len_of_chunk]
-
-
 def reducir_contadores(data1, data2):
     for key, value in data2.items():
         if key in data1.keys():
-            data1[key].update(data2[key])
+            data1.update({key: data2[key]})
         else:
             data1.update({key: value})
     return data1
+
 
 def separar_tags(data):
     for key in data.items():
         return dict([(key[0], key[1])])
     # return dict([(tag[0], tag[1]) for tag in data.items()])
 
-datos_cortados = chunkify(root, 10)
-mapped = list(map(respuestas_sin_respuesta, root))
-mapped1 = list(filter(None, mapped))
-tag_palabra= list(map(separar_tags, mapped1))
-reducido = reduce(reducir_contadores, tag_palabra)
+
+def get_10_sin_repuestas():
+    base = str(os.path.dirname(__file__)) + '/'
+    tree = ET.parse(base + 'posts.xml')
+    root = tree.getroot()
+    mapped = list(map(post_sin_respuesta, root))
+    mapped = list(filter(None, mapped))
+    reducido = reduce(reducir_contadores, mapped)
+    return reducido.most_common(10)
+
+
+top_10 = get_10_sin_repuestas()
+print(top_10)
