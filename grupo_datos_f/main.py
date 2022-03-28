7a2069d1c7e38d4b6b65b1f5d30c6ceb3c734b9d
penalozale <penaloza.le@gmail.com>
2022-03-28 15:55:04 -0300

    Add second task and reordered functions

--- grupo_datos_f/main.py
+++ grupo_datos_f/main.py
@@ -4,6 +4,7 @@ import xml.etree.ElementTree as ET
 from datetime import datetime
 from collections import Counter
 from functools import reduce
+import re
 
 # Loading logging configuration file
 logging.config.fileConfig(cfg('LOG_PATH'))
@@ -11,6 +12,7 @@ logging.config.fileConfig(cfg('LOG_PATH'))
 # Create Logger
 logger = logging.getLogger('Grupo_Datos_F')
 
+# --- General functions --- 
 def chunkify(iterable, len_of_chunk):
     '''
     Split an iterable into evenly sized Chunks
@@ -18,6 +20,21 @@ def chunkify(iterable, len_of_chunk):
     for i in range(0, len(iterable), len_of_chunk):
         yield iterable[i:i + len_of_chunk]
 
+def parse_xml():
+    '''
+    Parse the file 'posts.xml', which contain posts data
+    '''
+    tree = ET.parse('posts.xml')
+    return tree.getroot()
+
+def reduce_counter(data1, data2):
+    '''
+    Join data between Counter objects
+    '''
+    data1.update(data2)
+    return data1
+
+# --- Top 10 dates with less posts --- 
 def get_dates(data):
     '''
     Get the posts creation date and create a date object from a string
@@ -28,30 +45,42 @@ def get_dates(data):
 
 def mapper(data):
     '''
-    Apply "mapper" functions
+    Apply "mapper" functions to get the posts dates
     '''
     mapped_dates = list(map(get_dates, data))
     return mapped_dates
 
-def reduce_counter(data1, data2):
-    '''
-    Join data
-    '''
-    data1.update(data2)
-    return data1
-
 def calculate_top_10_less(data):
+    '''
+    Select only the top 10 most used words
+    '''
     top_10 = dict(data.most_common()[-10:])
     top_10 = list(top_10.keys())
     return top_10
 
-# Parse the file with posts data
-tree = ET.parse('posts.xml')
-root = tree.getroot()
-# Create chunks
-data_chunks = chunkify(root, 20)
+# --- Top 10 most used words in posts --- 
+def words_counter(data):
+    '''
+    Count, and collect, the number of words in each post 
+    '''
+    body = data.attrib['Body']
+    body = re.findall('(?<!\S)[A-Za-z]+(?!\S)|(?<!\S)[A-Za-z]+(?=:(?!\S))',body)
+    words = Counter(body)
+    return words
 
+def mapper_words(data):
+    '''
+    Apply "mapper" functions to get the most used words in posts
+    '''
+    mapped_words = list(map(words_counter, data))
+    reduced = reduce(reduce_counter, mapped_words)
+    return reduced
+
+
+# --- Tasks to resolve --- 
 def top_10_dates_less_post():
+    root = parse_xml()
+    data_chunks = chunkify(root, 20)
     # Apply mapper and reducer functions
     mapped = list(map(mapper, data_chunks))
     mapped = list(map(Counter, mapped))
@@ -64,6 +93,18 @@ def top_10_dates_less_post():
     logging.info(' Top 10 dates with less posts - Downloaded')
     return
 
+def top_10_words():
+    root = parse_xml()
+    data_chunks = chunkify(root, 20)
+    mapped_words = list(map(mapper_words, data_chunks))
+    reduced_words = reduce(reduce_counter, mapped_words)
+    print('\nTop 10 most used words in posts:')
+    print(reduced_words.most_common(10))
+    logging.info(' Top 10 most used words in posts - Downloaded')
+    return
+
+
 # Run main code
 if __name__ == '__main__':
     top_10_dates_less_post()
+    top_10_words()
